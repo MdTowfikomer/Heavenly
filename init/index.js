f7d902bdf9f640d0282309bc1171c98141abab0f
Md Towfik Omer <mdtowfikomer@gmail.com>
2026-02-09 15:13:58 +0530

    added listings data

--- init/index.js
+++ init/index.js
@@ -1,10 +1,13 @@
+const path = require("path");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data");
 const Listing = require("../models/listing");
 
 // setup the mongo DB connection
 
-const MONGODB_URL = "mongodb://127.0.0.1:27017/Havenly";
+// const MONGODB_URL = "mongodb://127.0.0.1:27017/Havenly"; // developement DB url
+const MONGODB_URL = process.env.ATLASDB_URL;  // cloude db url
 
 async function main() {
     mongoose.connect(MONGODB_URL);
@@ -20,7 +23,7 @@ main()
 
 const initDB = async () =>{
     await Listing.deleteMany({});
-    await Listing.insertMany(initData.data.map((obj) => ({...obj, owner: "697cc89e4144be61b7f2799f"})));
+    await Listing.insertMany(initData.data.map((obj) => ({...obj, owner: "6989a600e27396cd85a21565"})));
     console.log("Data was initialized");
 }
 
